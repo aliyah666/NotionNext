47c085ec7a0cfddb2b8958ab7d8dc9af661ae699
aliyah666 <56624732+aliyah666@users.noreply.github.com>
2026-04-18 23:46:59 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -20,7 +20,7 @@ const BLOG = {
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客,Savvy Travel,Hardy', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
-  BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '© 2026 Savvy Travel. As an affiliate, I may earn a commission from qualifying purchases.', // 备案号 闽ICP备XXXXXX
+  BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '© 2026 Savvy Travel.<br/><span style="opacity: 0.6; font-size: 0.85em; display: block; margin-top: 5px;">As an affiliate, I may earn a commission from qualifying purchases. | <span style="text-decoration: underline;">Privacy Policy</span></span>', // 备案号 闽ICP备XXXXXX
   BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || '/privacy', // 备案查询链接，如果用了萌备等备案请在这里填写
   BEI_AN_GONGAN: process.env.NEXT_PUBLIC_BEI_AN_GONGAN || '', // 公安备案号，例如 '浙公网安备3xxxxxxxx8号'
 
